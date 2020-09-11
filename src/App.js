39bd60a7f7cd77298901dead9d52da349cb67906
unknown <nestorovicmilan1@gmail.com>
2020-09-11 11:22:37 +0200

    Fetching data, folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      "https://my-json-server.typicode.com/brankostancevic/products/products"
+    );
+    this.setState({ products: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
